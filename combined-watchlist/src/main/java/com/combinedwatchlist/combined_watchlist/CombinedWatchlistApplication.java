bdebdf577a520108df6c944999d85352bdd6df44
erikkb <erik-brandes@web.de>
2025-01-21 12:22:10 +0100

    spring initializr

---
+++ combined-watchlist/src/main/java/com/combinedwatchlist/combined_watchlist/CombinedWatchlistApplication.java
@@ -0,0 +1,13 @@
+package com.combinedwatchlist.combined_watchlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CombinedWatchlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CombinedWatchlistApplication.class, args);
+	}
+
+}
